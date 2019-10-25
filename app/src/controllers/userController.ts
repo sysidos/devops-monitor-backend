5d350ecbeac86600fc579d3869309a5043c3282d
Robin Ambrosius <ambrosius@dezember.co>
2019-10-25 12:52:20 +0300

    use async/await instead of callbacks

--- app/src/controllers/userController.ts
+++ app/src/controllers/userController.ts
@@ -6,52 +6,55 @@ const User = mongoose.model('User', UserSchema);
 
 export class UserController {
 
-    public index (req: Request, res: Response) {
-        User.find({}, (err, user) => {
-            if(err){
-                res.send(err);
-            }
-            res.json(user);
-        });
+    public async index (req: Request, res: Response) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch(err) {
+            res.send(err);
+        }
     }
 
-    public create (req: Request, res: Response) {
-        let newUser = new User(req.body);
-
-        newUser.save((err, user) => {
-            if(err){
-                res.send(err);
-            }
+    public async create (req: Request, res: Response) {
+        try {
+            let newUser = new User(req.body);
+            const user = await newUser.save();
             res.json(user);
-        });
+        } catch(err) {
+            res.send(err);
+        }
     }
 
 
-    public find (req: Request, res: Response) {
-        User.findById(req.params.userId, (err, user) => {
-            if(err){
-                res.send(err);
-            }
+    public async find (req: Request, res: Response) {
+        try {
+            const user = await User.findById(req.params.userId);
             res.json(user);
-        });
+        } catch(err) {
+            res.send(err);
+        }
     }
 
-    public update (req: Request, res: Response) {
-        User.findOneAndUpdate({ _id: req.params.userId }, req.body, { new: true }, (err, user) => {
-            if(err){
-                res.send(err);
-            }
+    public async update (req: Request, res: Response) {
+        try {
+            const user = await User.findOneAndUpdate({
+                _id: req.params.userId
+            }, req.body, { new: true });
             res.json(user);
-        });
+        } catch(err) {
+            res.send(err);
+        }
     }
 
-    public delete (req: Request, res: Response) {
-        User.remove({ _id: req.params.userId }, (err, user) => {
-            if(err){
-                res.send(err);
-            }
+    public async delete (req: Request, res: Response) {
+        try {
+            await User.deleteOne({
+                _id: req.params.userId
+            });
             res.json({ message: 'Successfully deleted user!'});
-        });
+        } catch(err) {
+            res.send(err);
+        }
     }
 
 }
