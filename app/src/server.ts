cfb5a714817df4a83fc6a78e7e8cd7734b00c41d
Robin Ambrosius <ambrosius@dezember.co>
2019-10-28 12:47:33 +0300

    use nginx proxy for ssl instead of nodejs config

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,13 +1,6 @@
 import app from './app';
-import https from 'https';
-import fs from 'fs';
 const PORT = 3000;
 
-const httpsOptions = {
-    key: fs.readFileSync('./config/key.pem'),
-    cert: fs.readFileSync('./config/cert.pem')
-}
-
-https.createServer(httpsOptions, app).listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
 })
