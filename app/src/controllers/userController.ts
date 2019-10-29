86c241f9cacde7701593b51a28dca4a8df5d6872
Robin Ambrosius <ambrosius@dezember.co>
2019-10-29 10:13:57 +0300

    add controller docblocks

--- app/src/controllers/userController.ts
+++ app/src/controllers/userController.ts
@@ -4,8 +4,17 @@ import { Request, Response } from 'express';
 
 const User = mongoose.model('User', UserSchema);
 
+/**
+ * CRUD-Controller for user models
+ */
 export class UserController {
 
+    /**
+     * lists all available users
+     * @param  req express request object
+     * @param  res express response object
+     * @return     json object with statusCode and list of users
+     */
     public async index (req: Request, res: Response) {
         try {
             const users = await User.find();
@@ -15,6 +24,12 @@ export class UserController {
         }
     }
 
+    /**
+     * creates a new user instance
+     * @param  req express request object with updated user details as parameters
+     * @param  res express response object
+     * @return     json object with statusCode and and created user
+     */
     public async create (req: Request, res: Response) {
         try {
             let newUser = new User(req.body);
@@ -25,7 +40,12 @@ export class UserController {
         }
     }
 
-
+    /**
+     * displays details of specific user instance
+     * @param  req express request object with userId parameter
+     * @param  res express response object
+     * @return     json object with statusCode and user details
+     */
     public async find (req: Request, res: Response) {
         try {
             const user = await User.findById(req.params.userId);
@@ -35,6 +55,12 @@ export class UserController {
         }
     }
 
+    /**
+     * updates a specific user instance
+     * @param  req express request object with userId parameter
+     * @param  res express response object
+     * @return     json object with statusCode and updated user details
+     */
     public async update (req: Request, res: Response) {
         try {
             const user = await User.findOneAndUpdate({
@@ -46,6 +72,12 @@ export class UserController {
         }
     }
 
+    /**
+     * deletes a specific user instance
+     * @param  req express request object with userId parameter
+     * @param  res express response object
+     * @return     json object with statusCode
+     */
     public async delete (req: Request, res: Response) {
         try {
             await User.deleteOne({
