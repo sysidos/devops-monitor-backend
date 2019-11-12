09366013e25c426ab8079a082cb4497ccb84c1bd
Robin Ambrosius <ambrosius@dezember.co>
2019-11-12 21:18:51 +0300

    hash password when creating user

--- app/src/models/userModel.ts
+++ app/src/models/userModel.ts
@@ -3,13 +3,21 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 export const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
   email: {
     type: String,
     required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: false
+  },
+  lastName: {
+    type: String,
+    required: false
   },
   createdDate: {
     type: Date,
